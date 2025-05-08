602d4523d8c4ea39ed0d7c8a3653d803b493594f
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-08 13:49:51 +0530

    mobile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -375,7 +375,7 @@ const Profile = () => {
                 strays near them. Hopefully.
               </div>
             ) : (
-              <div className="columns-2  sm:columns-2 lg:columns-3 sm:gap-4 gap-1 space-y-1 sm:space-y-4">
+              <div className="columns-2  sm:columns-2 lg:columns-3 sm:gap-4 gap-2 space-y-3 sm:space-y-4">
                 {dogsData.map((dog) => (
                   <div
                     key={dog._id}
