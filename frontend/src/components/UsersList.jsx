d6c61328fd8d941664d66d97b2a70a6d22dcd406
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-09 01:14:35 +0530

    profile posts loader added

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -50,7 +50,7 @@ const UsersList = () => {
 
       <div className="relative z-10 max-w-4xl mx-auto flex-1 w-full">
         <div className="bg-white rounded-xl shadow-md overflow-hidden sm:p-1 p-0.5 h-full min-h-[calc(100vh-4rem)] flex flex-col">
-          <h1 className="text-2xl font-bold text-gray-800 mb-4 mt-5 pl-4">
+          <h1 className="text-2xl font-bold text-violet-600 mb-4 mt-5 pl-4">
             Community
           </h1>
 
