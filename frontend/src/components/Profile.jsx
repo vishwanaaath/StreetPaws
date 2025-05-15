744b247d1d93dc114ab44e5883800f4716816bfd
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-15 18:43:59 +0530

    changed profile layout

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -217,7 +217,7 @@ const Profile = () => {
         <div className="bg-white rounded-xl shadow-md overflow-hidden min-h-screen sm:p-6 p-2.5 ">
           {/* Profile Header Section */}
           <div className="flex flex-col md:flex-row items-start md:items-center sm:gap-6 gap-1 sm:mb-8 mb-4">
-            <div className="flex items-center w-full md:w-auto mt-4 gap-0">
+            <div className="flex items-center w-full md:w-auto mt-2 gap-0">
               {/* Profile Picture */}
               <div className="w-24 h-24 sm:w-26 sm:h-26 rounded-full ml-2 bg-gray-200 overflow-hidden">
                 <img
