f50b18fb0b19953ca5dbbf6b4525ef5a6cebf766
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-15 15:15:05 +0530

    changed profile layout

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -245,7 +245,7 @@ const Profile = () => {
             </div>
 
             <div className="w-full md:w-auto">
-              <div className="flex items-center sm:mb-2 mb-0 gap-2">
+              <div className="flex items-center justify-center sm:mb-2 mb-0 gap-2">
                 <h1 className="text-2xl font-bold text-gray-800 whitespace-nowrap overflow-hidden text-ellipsis max-w-[250px] sm:max-w-none">
                   {currentUser.username}
                 </h1>
