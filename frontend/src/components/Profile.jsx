89b794a1f9bf6fc1a267be2a4e4cb0ba17d7c6fd
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-15 14:41:11 +0530

    changed profile layout

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -233,7 +233,7 @@ const Profile = () => {
 
               {/* Mobile-only stats - tightly next to DP */}
               <div className="md:hidden flex flex-col justify-center items-center text-center  pl-6">
-                <p className="text-xl font-bold text-violet-600 leading-tight">
+                <p className="text-xl font-extrabold text-violet-600 leading-tight">
                   {currentUser.dogsListed.length}
                 </p>
                 <p className="text-xs text-gray-600 leading-tight">
