a4a12d6f86487f8ae1aac68e7ae50db29328f8a8
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-16 23:47:18 +0530

    address added

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -423,7 +423,7 @@ const MapView = () => {
                             <span className="text-[14px] font-semibold text-gray-800">
                               {distance.split(" ")[0]}
                             </span>
-                            <span className="text-[10px] font-medium text-gray-500">
+                            <span className="text-[8px] font-medium text-gray-500">
                               {distance.split(" ")[1]}
                             </span>
                             <span className="text-[10px] text-gray-400 ml-1">
@@ -517,7 +517,7 @@ const MapView = () => {
                         </div>
                       )}
 
-                      
+
                     </div>
                   </div>
                 </Popup>
