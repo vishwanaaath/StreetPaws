6ac590ea317940b9cdebdd76e6cbda285357b3b7
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-20 20:58:51 +0530

    explore actions

--- frontend/src/components/explore.jsx
+++ frontend/src/components/explore.jsx
@@ -358,7 +358,7 @@ const Explore = () => {
   );
 
   return (
-    <div className="p-2 sm:p-4">
+    <div className="p-2 sm:p-4" {...handlers}>
       {/* Error Message Toast */}
       {errorMessage && (
         <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded-md shadow-lg z-50">
