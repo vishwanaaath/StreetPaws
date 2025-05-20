5a36e8b8861c3ae460bac80deae7eb98284bb468
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-05-20 13:43:45 +0530

    explore swipe

--- frontend/src/components/UserLoader.jsx
+++ frontend/src/components/UserLoader.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Search } from "lucide-react";
 
 const UserLoader = () => {
   return (
@@ -8,25 +7,28 @@ const UserLoader = () => {
 
       <div className="relative z-10 max-w-4xl mx-auto flex-1 w-full">
         <div className="bg-white rounded-xl shadow-md overflow-hidden sm:p-1 p-0.5 h-full min-h-[calc(100vh-4rem)] flex flex-col">
-          <h1 className="text-2xl font-bold text-violet-600 mb-2 mt-5 pl-4 flex justify-between items-center pr-4">
+          <h1 className="text-[25px] font-bold text-violet-600 mb-2 mt-4 pl-4 flex justify-between items-center pr-4">
             <div className="flex items-center gap-2">
-              {/* Left arrow skeleton loader */}
-              <div className="w-6 h-6 rounded-full bg-gray-200 animate-pulse mr-1" />
-              Community
+              <div className="w-6 h-6 bg-gray-300 rounded-full animate-pulse" />
+              <div className="h-4 w-24 bg-gray-300 rounded animate-pulse" />
             </div>
-            <button className="text-violet-600">
-              <Search size={20} />
-            </button>
+            <div className="w-6 h-6 bg-gray-300 rounded-full animate-pulse" />
           </h1>
+
+          <div className="px-4 mt-2 mb-2">
+            <div className="h-10 bg-gray-200 rounded-lg animate-pulse" />
+          </div>
+
           <div className="flex-1 overflow-y-auto space-y-1 pb-1">
-            {[...Array(12)].map((_, index) => (
+            {[...Array(6)].map((_, idx) => (
               <div
-                key={index}
-                className="group relative flex items-center ml-2 p-2 sm:p-4 bg-white">
-                <div className="w-14 h-14 sm:w-14 sm:h-14 rounded-full bg-gray-200 animate-pulse" />
+                key={idx}
+                className="relative flex items-center ml-2 p-2 sm:p-4 bg-white">
+                <div className="relative w-14 h-14 sm:w-14 sm:h-14 rounded-full bg-gray-200 animate-pulse" />
+
                 <div className="ml-4 flex-1 min-w-0 space-y-2">
-                  <div className="h-4 bg-gray-200 rounded w-32 animate-pulse" />
-                  <div className="h-3 bg-gray-200 rounded w-24 animate-pulse" />
+                  <div className="h-4 w-1/2 bg-gray-300 rounded animate-pulse" />
+                  <div className="h-3 w-1/4 bg-gray-200 rounded animate-pulse" />
                 </div>
               </div>
             ))}
